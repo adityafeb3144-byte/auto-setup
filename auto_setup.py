3cb1c623a3f3bab35f6d407579176852958ffbbe
adityafeb3144-byte <adityafeb3144@gmail.com>
2026-02-17 08:01:00 +0530

    exception handling is added
    exception handling is done

--- auto_setup.py
+++ auto_setup.py
@@ -2,14 +2,24 @@ import os
 import subprocess
 import shlex
 
-name_dirt = input("Enter a Directory name : ")
-
-os.mkdir(f"{name_dirt}")
-
+while True:
+    name_dirt = input("Enter a Directory name : ")
+    try :
+        os.mkdir(f"{name_dirt}")
+        break
+    except FileExistsError :
+        print("Directory already exists ")
+        print("Please try again")
+        
 data = open(f"{name_dirt}/solution.py","w")
 data.close()
 
 os.chdir(f"{name_dirt}")
-import subprocess
-subprocess.run(shlex.split("git init"))
-subprocess.run(shlex.split("git add ."))
+try :
+    subprocess.run(shlex.split("git init"))
+    subprocess.run(shlex.split("git add ."))
+except :
+    print("Git is not installed in the device")
+    permission = input("Do you want to install Git [Y/N] : ")
+    if permission.lower() == "y":
+        subprocess.run(shlex.split(""))
