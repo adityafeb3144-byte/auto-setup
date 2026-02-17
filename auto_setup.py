86bd4d492179ab0f469e56ef54c5a0bf2f2668ac
adityafeb3144-byte <adityafeb3144@gmail.com>
2026-02-17 19:14:17 +0530

    Improve Git installation handling in auto_setup.py
    Added exception handling for Git installation process based on the user's device.

--- auto_setup.py
+++ auto_setup.py
@@ -15,12 +15,20 @@ data = open(f"{name_dirt}/solution.py","w")
 data.close()
 
 os.chdir(f"{name_dirt}")
-try :
-    subprocess.run(shlex.split("git init"))
-    subprocess.run(shlex.split("git add ."))
-except :
-    print("Git is not installed in the device")
-    permission = input("Do you want to install Git [Y/N] : ")
-    if permission.lower() == "y":
-
-        subprocess.run(shlex.split("exception handling is done"))
+while True:
+    try :
+        subprocess.run(shlex.split("git init"))
+        subprocess.run(shlex.split("git add ."))
+        break
+    except :
+        print("Git is not installed in the device")
+        permission = input("Do you want to install Git [Y/N] : ")
+        if permission.lower() == "y":
+            device = input("Enter your input for device \nWindows : W\nmacOS : M\nLinux : N")
+            if device.lower() == "w":
+                subprocess.run(shlex.split("winget install --id Git.Git -e --source winget"))
+            elif device.lower() == "m":
+                subprocess.run(shlex.split("brew install git"))
+            else:
+                subprocess.run(shlex.split("sudo apt-get update"))
+                subprocess.run(shlex.split("sudo apt-get install git"))
